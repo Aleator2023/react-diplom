dcdf4e3639acb827d96578f97b0a37c2d17b14ee
Aleator2023 <136687310+Aleator2023@users.noreply.github.com>
2024-04-23 14:34:20 +0300

    diplom 1.4

--- my-shoe-store/src/components/CatalogComponent.jsx
+++ my-shoe-store/src/components/CatalogComponent.jsx
@@ -1,48 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import { useCategories, useItems } from './apiHooks';
-import SearchComponent from './SearchComponent';
 
-const CatalogComponent = () => {
+const CatalogComponent = ({ searchQuery }) => {
   const { categories, loading: loadingCategories, error: errorCategories } = useCategories();
   const [selectedCategoryId, setSelectedCategoryId] = useState(null);
-  const [searchQuery, setSearchQuery] = useState(''); // Добавляем состояние для поискового запроса
   const [offset, setOffset] = useState(0);
   const { items, loading: loadingItems, error: errorItems, hasMore } = useItems(selectedCategoryId, searchQuery, offset);
 
   useEffect(() => {
-    setOffset(0); // Сбросить offset при смене категории или поискового запроса
-  }, [selectedCategoryId, searchQuery]); // Добавляем searchQuery в массив зависимостей
-
-  const handleSearch = (searchTerm) => {
-    setSearchQuery(searchTerm); // Устанавливаем поисковый запрос
-  };
+    setOffset(0); // Сброс offset при смене категории или поискового запроса
+  }, [selectedCategoryId, searchQuery]);
 
   const handleCategoryChange = (categoryId) => {
     setSelectedCategoryId(categoryId);
   };
 
   const handleLoadMore = () => {
-    setOffset(prevOffset => prevOffset + 6); // Увеличиваем offset для подгрузки следующих элементов
+    setOffset(prevOffset => prevOffset + 6);
   };
 
   return (
     <section className="catalog">
       <h2 className="text-center">Каталог</h2>
-
-      <SearchComponent onSearch={handleSearch} />
-
       {loadingCategories && <div className="preloader">Загрузка категорий...</div>}
       {errorCategories && <div>Error: {errorCategories.message}</div>}
       <div>
-      {categories.map(category => (
-  <button
-    key={category.id}
-    onClick={() => handleCategoryChange(category.id)}
-    className={`category-button ${selectedCategoryId === category.id ? 'active' : ''}`}
-  >
-    {category.title}
-  </button>
-))}
+        {categories.map(category => (
+          <button
+            key={category.id}
+            onClick={() => handleCategoryChange(category.id)}
+            className={`category-button ${selectedCategoryId === category.id ? 'active' : ''}`}
+          >
+            {category.title}
+          </button>
+        ))}
       </div>
       {loadingItems && <div className="preloader">Загрузка товаров...</div>}
       {errorItems && <div>Error: {errorItems.message}</div>}
--- my-shoe-store/src/components/CatalogPage.jsx
+++ my-shoe-store/src/components/CatalogPage.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 import CatalogComponent from './CatalogComponent';
 import Footer from './Footer';
+import SearchComponent from './SearchComponent';
 
-function CatalogPage () {
-    return (
-        <div>
-          <Header />
-          <CatalogComponent />
-          <Footer />
-        </div>
-      );
-    }
+function CatalogPage() {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSearch = (searchTerm) => {
+    setSearchQuery(searchTerm);
+  };
+
+  return (
+    <div>
+      <Header />
+      <h2 className="text-center">Каталог</h2>
+      <SearchComponent onSearch={handleSearch} />
+      <CatalogComponent searchQuery={searchQuery} />
+      <Footer />
+    </div>
+  );
+}
 
 export default CatalogPage;
